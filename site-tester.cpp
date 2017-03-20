f78eb14535b13d394891bc65eb675fd7443f0ef3
Noah Andrew Sarkey <nsarkey@student02.cse.nd.edu>
2017-03-20 19:54:38 -0400

    Set up file

--- site-tester.cpp
+++ site-tester.cpp
@@ -5,12 +5,18 @@
 #include<stdio.h>
 using namespace std;
 
-int main ()
+int main (int argc, char argv[])
 {
-
-
-
-
-
-
+	if (argc != 1) {
+		cout << "Error. Invalid number of arguments." << endl;
+	}
+	else {
+		run(argv[1]);
+	}
+	
+	return 0;
 }	//end of main function
+
+void run(char a[]) {
+
+}	//end of run function
